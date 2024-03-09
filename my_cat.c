bec7aeb0f4915d84e66f00fa0ffc38377c8387b4
baibsFar <fararano.kevin@gmail.com>
2024-03-09 14:24:51 +0300

    feat: program status return on error

--- my_cat.c
+++ my_cat.c
@@ -32,6 +32,7 @@ int	main(int ac, char **av)
 {
 	int	i;
 	int	fd;
+	int	return_status;
 
 	if (ac < 2)
 	{
@@ -39,11 +40,16 @@ int	main(int ac, char **av)
 		exit(EXIT_FAILURE);
 	}
 	i = 1;
+	return_status = 0;
 	while (i < ac)
 	{
 		fd = open(av[i], O_RDONLY);
 		if (fd <= 0)
 		{
+			putstr_fd("my_cat: ", 1);
+			putstr_fd(av[i], 1);
+			putstr_fd(": No such file or directory\n", 1);
+			return_status = 1;
 			i++;
 			continue ;
 		}
@@ -51,4 +57,5 @@ int	main(int ac, char **av)
 		close(fd);
 		i++;
 	}
+	return (return_status);
 }
